064452f054d3cb152c08e1cca781dce6058ccca1
浅小沫 <67664815+Truimo@users.noreply.github.com>
2024-10-20 01:35:58 +0800

    fix: update friend's link

--- src/config.ts
+++ src/config.ts
@@ -143,8 +143,8 @@ export const siteConfig = {
     },
     {
       title: "浅小沫",
-      icon: "https://www.xdm.me/_next/image?url=%2Favatar.jpg&w=64&q=75",
-      link: "https://www.xdm.me/",
+      icon: "https://www.qxm.me/_next/image?url=%2Favatar.jpg&w=64&q=75",
+      link: "https://www.qxm.me/",
       description: "Meeting you is the best of the best.",
     },
   ],
